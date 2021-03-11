3161d226338ddab52186e168fd19c4319994a0d5
Grzegorz <karaluch26@wp.pl>
2021-03-11 19:11:19 +0100

    Trello Conf

---
+++ tasks/src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
+
+@RequiredArgsConstructor
+@Component
+public class TrelloClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiPoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiPoint + "/members/me/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .build()
+                .encode()
+                .toUri();
+        TrelloBoardDto[] boardResponse = restTemplate.getForObject
+                (url, TrelloBoardDto[].class);
+        if (boardResponse != null) {
+            return Optional.ofNullable(boardResponse)
+                    .map(Arrays::asList)
+                    .orElse(Collections.emptyList());
+        }
+        return new ArrayList<>();
+    }
+}
