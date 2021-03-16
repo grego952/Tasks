065876844174d1073b791bf7efd1331e7ddaa620
Grzegorz <karaluch26@wp.pl>
2021-03-16 19:31:39 +0100

    First attempt on Heroku

--- tasks/src/main/java/com/crud/tasks/TasksApplication.java
+++ tasks/src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
 	}
 }
