9fe4c50a0fc7a8543b19efe179f62255bc9b21e8
grego952 <62639546+grego952@users.noreply.github.com>
2021-05-01 12:56:59 +0200

    Trello Controller tests

--- tasks/src/main/java/com/crud/tasks/controller/TrelloController.java
+++ tasks/src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,11 +3,11 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.facade.TrelloFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
